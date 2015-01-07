315c2b35f9568a3c51039fe413a0d3bfbd7cab2f
Andres Smiith <smiith17@gmail.com>
2015-01-06 23:47:38 -0430

    Agregando el proyecto al repositorio
    Todos los archivos realizados hasta el momento.

---
+++ alpha 0.2/Assets/Recursos survival/scripts/agua.js
@@ -0,0 +1,18 @@
+#pragma strict
+
+private var survivalScript : Survivalcontroller;
+
+function Start () 
+{
+	survivalScript = GameObject.Find("First Person Controller").GetComponent(Survivalcontroller);
+}
+
+function OnTriggerStay (Col : Collider)
+{
+    if( (Col.tag == "Player") && (Input.GetKey(KeyCode.E)) )
+    {
+        Destroy(gameObject);
+        survivalScript.currentHealth += 25;
+        
+    }
+}
---
+++ alpha 0.2/Assets/Recursos survival/scripts/botiquin.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+private var survivalScript : Survivalcontroller;
+
+function Start ()
+{
+    survivalScript = GameObject.Find("First Person Controller").GetComponent(Survivalcontroller);
+}
+
+function OnTriggerStay (Col : Collider)
+{
+    if( (Col.tag == "Player") && (Input.GetKey(KeyCode.E)) )
+    {
+        Destroy(gameObject);
+        survivalScript.currentHealth += 25;
+        
+    }
+}
